e133a81a7f617b97b08aeb23190f8c422654677b
Arjun Ramesh <arjun.ramesh77@gmail.com>
2020-10-18 18:05:53 -0400

    two rounds working
    Repeated functions due to unrolling
    third round compilation issue.

--- marlowe_contracts/savingsBoxContract.hs
+++ marlowe_contracts/savingsBoxContract.hs
@@ -46,43 +46,42 @@ waitingPeriod slots = Slot (slots + 90)
 claimRewards :: Integer -> Contract
 claimRewards slots =    If (winObservation 0 0 0)
                             (When [
-                                Case kevinChoice (kevinWon slots), 
-                                Case mikeChoice (mikeWon slots), 
-                                Case arjunChoice (arjunWon slots)] 
+                                Case kevinChoice (kevinWon slots 1), 
+                                Case mikeChoice (mikeWon slots 1), 
+                                Case arjunChoice (arjunWon slots 1)] 
                                 (choosingPeriod slots)
                                 Close)
                         (If (winObservation 1 0 0)
                              (When [
-                                Case mikeChoice (mikeWon slots), 
-                                Case arjunChoice (arjunWon slots)] 
+                                Case mikeChoice (mikeWon slots 2), 
+                                Case arjunChoice (arjunWon slots 2)] 
                                 (choosingPeriod slots)
                                 Close)
                         (If (winObservation 0 1 0)
                              (When [
-                                Case kevinChoice (kevinWon slots), 
-                                Case arjunChoice (arjunWon slots)] 
+                                Case kevinChoice (kevinWon slots 2), 
+                                Case arjunChoice (arjunWon slots 2)] 
                                 (choosingPeriod slots) 
                                 Close)
                         (If (winObservation 0 0 1)
                             (When [
-                                Case kevinChoice (kevinWon slots), 
-                                Case mikeChoice (mikeWon slots)] 
+                                Case kevinChoice (kevinWon slots 2), 
+                                Case mikeChoice (mikeWon slots 2)] 
                                 (choosingPeriod slots) 
                                 Close)
                         (If (winObservation 1 1 0)
                             (When [
-                                Case arjunChoice (arjunWon slots)] 
+                                Case arjunChoice (arjunWon slots 3)] 
                                 (choosingPeriod slots) 
                                 Close)
                         (If (winObservation 0 1 1)
                             (When [
-                                Case mikeChoice (mikeWon slots), 
-                                Case arjunChoice (arjunWon slots)] 
+                                Case kevinChoice (kevinWon slots 3)] 
                                 (choosingPeriod slots)
                                 Close)
                         (If (winObservation 1 0 1)
                             (When [
-                                Case mikeChoice (mikeWon slots)] 
+                                Case mikeChoice (mikeWon slots 3)] 
                                 (choosingPeriod slots) 
                                 Close)
                         (Close)))))))
@@ -98,25 +97,20 @@ choosingPeriod :: Integer -> Slot
 choosingPeriod slots = Slot (slots + 100)
 
 {- kevin gets paid -}
-kevinWon :: Integer -> Contract
-kevinWon slots =Pay "kevin" (Party "kevin") ada depositAmount  
-                    (Pay "mike" (Party "kevin") ada depositAmount 
-                        (Pay "arjun" (Party "kevin") ada depositAmount 
-                            ((Let (ValueId "kevin-won") (Constant 1) Close))))
+kevinWon :: Integer -> Integer -> Contract
+kevinWon slots round =  Pay "kevin" (Party "kevin") ada depositAmount  
+                            (Pay "mike" (Party "kevin") ada depositAmount 
+                                (Pay "arjun" (Party "kevin") ada depositAmount 
+                                    ((Let (ValueId "kevin-won") (Constant 1) (gameLoop1 100)))))
+mikeWon slots round =   Pay "mike" (Party "mike") ada depositAmount
+                            (Pay "kevin" (Party "mike") ada depositAmount 
+                                (Pay "arjun" (Party "mike") ada depositAmount
+                                    ((Let (ValueId "mike-won") (Constant 1) (gameLoop1 100)))))
 
-{- mike gets paid -}
-mikeWon :: Integer -> Contract
-mikeWon slots = Pay "mike" (Party "mike") ada depositAmount
-                    (Pay "kevin" (Party "mike") ada depositAmount 
-                        (Pay "arjun" (Party "mike") ada depositAmount
-                            ((Let (ValueId "mike-won") (Constant 1) Close))))
-
-{- arjun gets paid -}
-arjunWon :: Integer -> Contract
-arjunWon slots =Pay "arjun" (Party "arjun") ada depositAmount 
-                    (Pay "kevin" (Party "arjun") ada depositAmount 
-                        (Pay "mike" (Party "arjun") ada depositAmount 
-                            ((Let (ValueId "arjun-won") (Constant 1) Close))))
+arjunWon slots round=   Pay "arjun" (Party "arjun") ada depositAmount 
+                            (Pay "kevin" (Party "arjun") ada depositAmount 
+                                (Pay "mike" (Party "arjun") ada depositAmount 
+                                    ((Let (ValueId "arjun-won") (Constant 1) (gameLoop1 100)))))
 
 {- magic number choices made by participants -}
 kevinChoice, mikeChoice, arjunChoice :: Action
@@ -142,3 +136,155 @@ magicNumber = 1
    for being eligible to play -}
 depositAmount :: (Value Observation)
 depositAmount = Constant 100
+
+{---------- ROUND 2 ----------}
+gameLoop1 :: Integer -> Contract
+gameLoop1 slots =    When [Case kevinDepositAction 
+                        (When [Case mikeDepositAction 
+                            (When [Case arjunDepositAction (afterDepositing1 slots)] 
+                                (depositTimeOut slots)
+                                Close)] 
+                        (depositTimeOut slots)
+                        Close)] 
+                    (depositTimeOut slots)
+                    Close
+
+{- wait for certain amount of slots before drawing the winners -}
+afterDepositing1 :: Integer -> Contract
+afterDepositing1 slots = When [] (waitingPeriod slots) (claimRewards1 slots)
+
+{- now time to claim the rewards -}
+claimRewards1 :: Integer -> Contract
+claimRewards1 slots =    If (winObservation 0 0 0)
+                            (When [
+                                Case kevinChoice (kevinWon1 slots 1), 
+                                Case mikeChoice (mikeWon1 slots 1), 
+                                Case arjunChoice (arjunWon1 slots 1)] 
+                                (choosingPeriod slots)
+                                Close)
+                        (If (winObservation 1 0 0)
+                             (When [
+                                Case mikeChoice (mikeWon1 slots 2), 
+                                Case arjunChoice (arjunWon1 slots 2)] 
+                                (choosingPeriod slots)
+                                Close)
+                        (If (winObservation 0 1 0)
+                             (When [
+                                Case kevinChoice (kevinWon1 slots 2), 
+                                Case arjunChoice (arjunWon1 slots 2)] 
+                                (choosingPeriod slots) 
+                                Close)
+                        (If (winObservation 0 0 1)
+                            (When [
+                                Case kevinChoice (kevinWon1 slots 2), 
+                                Case mikeChoice (mikeWon1 slots 2)] 
+                                (choosingPeriod slots) 
+                                Close)
+                        (If (winObservation 1 1 0)
+                            (When [
+                                Case arjunChoice (arjunWon1 slots 3)] 
+                                (choosingPeriod slots) 
+                                Close)
+                        (If (winObservation 0 1 1)
+                            (When [
+                                Case kevinChoice (kevinWon1 slots 3)] 
+                                (choosingPeriod slots)
+                                Close)
+                        (If (winObservation 1 0 1)
+                            (When [
+                                Case mikeChoice (mikeWon1 slots 3)] 
+                                (choosingPeriod slots) 
+                                Close)
+                        (Close)))))))
+
+{- kevin gets paid -}
+kevinWon1 :: Integer -> Integer -> Contract
+kevinWon1 slots round =  Pay "kevin" (Party "kevin") ada depositAmount  
+                            (Pay "mike" (Party "kevin") ada depositAmount 
+                                (Pay "arjun" (Party "kevin") ada depositAmount 
+                                    ((Let (ValueId "kevin-won") (Constant 1) (gameLoop2 200)))))
+mikeWon1 slots round =   Pay "mike" (Party "mike") ada depositAmount
+                            (Pay "kevin" (Party "mike") ada depositAmount 
+                                (Pay "arjun" (Party "mike") ada depositAmount
+                                    ((Let (ValueId "mike-won") (Constant 1) (gameLoop2 200)))))
+
+arjunWon1 slots round=   Pay "arjun" (Party "arjun") ada depositAmount 
+                            (Pay "kevin" (Party "arjun") ada depositAmount 
+                                (Pay "mike" (Party "arjun") ada depositAmount 
+                                    ((Let (ValueId "arjun-won") (Constant 1) (gameLoop2 200)))))
+
+{---------- ROUND 3 ----------}
+gameLoop2 :: Integer -> Contract
+gameLoop2 slots =    When [Case kevinDepositAction 
+                        (When [Case mikeDepositAction 
+                            (When [Case arjunDepositAction (afterDepositing2 slots)] 
+                                (depositTimeOut slots)
+                                Close)] 
+                        (depositTimeOut slots)
+                        Close)] 
+                    (depositTimeOut slots)
+                    Close
+
+{- wait for certain amount of slots before drawing the winners -}
+afterDepositing2 :: Integer -> Contract
+afterDepositing2 slots = When [] (waitingPeriod slots) (Close)
+
+{- now time to claim the rewards -}
+claimRewards2 :: Integer -> Contract
+claimRewards2 slots =    If (winObservation 0 0 0)
+                            (When [
+                                Case kevinChoice (kevinWon2 slots 1), 
+                                Case mikeChoice (mikeWon2 slots 1), 
+                                Case arjunChoice (arjunWon2 slots 1)] 
+                                (choosingPeriod slots)
+                                Close)
+                        (If (winObservation 1 0 0)
+                             (When [
+                                Case mikeChoice (mikeWon2 slots 2), 
+                                Case arjunChoice (arjunWon2 slots 2)] 
+                                (choosingPeriod slots)
+                                Close)
+                        (If (winObservation 0 1 0)
+                             (When [
+                                Case kevinChoice (kevinWon2 slots 2), 
+                                Case arjunChoice (arjunWon2 slots 2)] 
+                                (choosingPeriod slots) 
+                                Close)
+                        (If (winObservation 0 0 1)
+                            (When [
+                                Case kevinChoice (kevinWon2 slots 2), 
+                                Case mikeChoice (mikeWon2 slots 2)] 
+                                (choosingPeriod slots) 
+                                Close)
+                        (If (winObservation 1 1 0)
+                            (When [
+                                Case arjunChoice (arjunWon2 slots 3)] 
+                                (choosingPeriod slots) 
+                                Close)
+                        (If (winObservation 0 1 1)
+                            (When [
+                                Case kevinChoice (kevinWon2 slots 3)] 
+                                (choosingPeriod slots)
+                                Close)
+                        (If (winObservation 1 0 1)
+                            (When [
+                                Case mikeChoice (mikeWon2 slots 3)] 
+                                (choosingPeriod slots) 
+                                Close)
+                        (Close)))))))
+
+{- kevin gets paid -}
+kevinWon2 :: Integer -> Integer -> Contract
+kevinWon2 slots round =  Pay "kevin" (Party "kevin") ada depositAmount  
+                            (Pay "mike" (Party "kevin") ada depositAmount 
+                                (Pay "arjun" (Party "kevin") ada depositAmount 
+                                    ((Let (ValueId "kevin-won") (Constant 1) (Close)))))
+mikeWon2 slots round =   Pay "mike" (Party "mike") ada depositAmount
+                            (Pay "kevin" (Party "mike") ada depositAmount 
+                                (Pay "arjun" (Party "mike") ada depositAmount
+                                    ((Let (ValueId "mike-won") (Constant 1) (Close)))))
+
+arjunWon2 slots round=   Pay "arjun" (Party "arjun") ada depositAmount 
+                            (Pay "kevin" (Party "arjun") ada depositAmount 
+                                (Pay "mike" (Party "arjun") ada depositAmount 
+                                    ((Let (ValueId "arjun-won") (Constant 1) (Close)))))
